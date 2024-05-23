16846783f494805347d28cef74d9883de8648d8a
sayed-mudassir <sayedmuddassir68@gmail.com>
2024-05-24 01:55:09 +0530

    initial commit

---
+++ option.js
@@ -0,0 +1,79 @@
+const activeCellElement = document.querySelector(".selected-cell")
+const form = document.getElementById("options-form")
+const exp = document.getElementById("expression")
+let selectedcell = null;
+const state = {};
+const defaultState = {
+    innertext : "",
+    isBold : false,
+    align : "left",
+    isUnderline : false,
+    isItalic : false,
+    fontSize : 16,
+    fontFaimly : "Sans Sherif",
+    textColor : "#000000",
+    backgroundColor : "#ffffff"
+}
+function applyCellInfoToForm(){
+    if(state[selectedcell.id]){
+        const data = state[selectedcell.id];
+        for(let key in data){
+            if(form[key].type === "checkbox"){
+                form[key].checked = data[key];
+            }
+            else{
+                form[key].value = data[key];
+            }
+        }
+    }
+    else{
+        form.reset();
+    }
+}
+
+function onFocusCell(e){
+    if(selectedcell){
+        selectedcell.classList.remove("active-cell");
+    }
+    selectedcell = e.target;
+    activeCellElement.innerText = selectedcell.id;
+    selectedcell.classList.add("active-cell");
+    applyCellInfoToForm();
+}
+function applyStylesToSelectedCell(styles){
+    selectedcell.style.fontFamily = styles.fontFaimly;
+    selectedcell.style.fontSize = styles.fontSize + "px";
+    selectedcell.style.fontWeight = styles.isBold ? "bold" : "normal";
+    selectedcell.style.fontStyle = styles.isItalic ? "italic" : "normal";
+    selectedcell.style.textDecoration = styles.isUnderline ? "underline" : "normal";
+    selectedcell.style.textAlign = styles.align;
+    selectedcell.style.color = styles.textColor;
+    selectedcell.style.backgroundColor = styles.backgroundColor;
+}
+form.addEventListener("change",(e)=>{
+    if(!selectedcell){
+        alert("plese select a cell");
+        form.reset();
+        return;
+    }
+    const formData = {
+        fontFaimly : form["fontFaimly"].value,
+        fontSize : form["fontSize"].value,
+        isBold : form["isBold"].checked,
+        isItalic : form["isItalic"].checked,
+        isUnderline : form["isUnderline"].checked,
+        align : form["align"].value,
+        textColor:form["textColor"].value,
+        backgroundColor : form["backgroundColor"].value
+    };
+    state[selectedcell.id] = {...formData,innerText: selectedcell.innerText}; 
+    applyStylesToSelectedCell(formData);
+});
+
+exp.addEventListener("keyup", (e)=>{
+    if(e.code === "Enter" && selectedcell){
+        let expression = exp.innerText;
+        let result = eval(`${exp.value}`);
+        selectedcell.innerText = result;
+    }
+})
---
+++ script.js
@@ -0,0 +1,32 @@
+const headRow = document.getElementById("head-row");
+const sno = document.getElementById("sno");
+const body = document.getElementById("body");
+const row = 100, columns = 26;
+for(let i = 1; i<=columns; i++){
+    const headCell = document.createElement("div");
+    if(i>0){
+    headCell.className = "col-head";
+    headCell.innerText = String.fromCharCode(i+64); // "A" => 65  
+    }
+    headRow.appendChild(headCell);
+}
+for(let i = 0; i<row;i++){
+    const snoCell = document.createElement("div");
+    snoCell.innerText = i+1;
+    snoCell.className = "sno-cell";
+    sno.appendChild(snoCell);
+}
+
+for(let i = 1; i<=row; i++){
+    const row = document.createElement("div");
+    row.className = "row"
+    for(let j = 1; j<=columns;j++){
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.contentEditable = true;
+        cell.id = `${String.fromCharCode(j+64)}${i}`;
+        cell.addEventListener("focus",onFocusCell);
+        row.appendChild(cell);
+    }
+    body.appendChild(row);
+}
