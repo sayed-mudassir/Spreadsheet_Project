ad9f5835376e94fc67e11bb39799170a00bfe8bc
sayed-mudassir <sayedmuddassir68@gmail.com>
2024-05-27 03:03:07 +0530

    Upload feature fixed

--- download-upload.js
+++ download-upload.js
@@ -1,39 +1,41 @@
 const download = document.getElementById("download");
 const upload = document.getElementById("upload");
 
-
+// function updateFormAccordingTheUploadFile(state,Changedcell){
+//     if(state[Changedcell.id]){
+//         const data = state[Changedcell.id];
+//         for(let i in data){
+//             debugger;
+//             if(i === "innerText" || i==="innertext") {
+//                 continue;
+//             }
+//             else if(form[i].type === "checkbox"){
+//                 form[i].checked = data[i];
+//             }
+//             else{
+//                 form[i].value = data[i];
+//             }
+//         }
+//     }
+//     form.reset();
+// } // this function is not working as per expection needed to be fixed 
 
 download.addEventListener("click", ()=>{
     const blob = new Blob([JSON.stringify(state)],{type: "application/json"});
     let url = URL.createObjectURL(blob);
     let link = document.createElement("a");
     link.href = url;
-    link.download = "spreedsheet".json;
+    link.download = "spreedsheet.json";
 
     link.click();
 });
 
-function updateCellInfoToForm(){
-    for(let key in state){
-        const data = state[key];
-        for(let key in data){
-            if(form[key].type === "checkbox"){
-                form[key].checked = data[key];
-            }
-            else{
-                form[key].value = data[key];
-            }
-        }
-    }
-    // else{
-    //     form.reset();
-    // }
-}
 
 function loadJson(fileData){
-     // Clear existing data
+    
     for(let key in fileData){
         let Changedcell = document.getElementById(key);
+        state[Changedcell.id] = fileData[key];
         Changedcell.innerText = fileData[key].innerText;
         Changedcell.style.fontFamily = fileData[key].fontFaimly;
         Changedcell.style.fontSize = fileData[key].fontSize + "px";
@@ -43,26 +45,8 @@ function loadJson(fileData){
         Changedcell.style.textAlign = fileData[key].align;
         Changedcell.style.color = fileData[key].textColor;
         Changedcell.style.backgroundColor = fileData[key].backgroundColor;
-        state[Changedcell.id] = fileData[key];
+        // updateFormAccordingTheUploadFile(state, Changedcell);
     }
-    // for(let k in state){
-    //     if(state[k]){
-    //         const data = state[k];
-    //         for(let key in data){
-    //             debugger;
-    //             if(form[key].type === "checkbox"){
-    //                 form[key].checked = data[key];
-    //             }
-    //             else{
-    //                 form[key].value = data[key];
-    //             }
-    //         }
-    //     }
-    //     else{
-    //         form.reset();
-    //     }
-    // }
-    
  }
 
 
